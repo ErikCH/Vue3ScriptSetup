3aacccc438030ae6e19a90bf18e0ce8199147a12
Erik Hanchett <erik.hanchett@gmail.com>
2021-09-05 16:02:43 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+let name = "bob123";
+let pokemon = ref(null) as any;
+
+async function pressed() {
+  const info = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  const json = await info.json();
+  pokemon.value = json;
+  console.log(pokemon.value);
+}
+</script>
+
+<template>
+  <HelloWorld :name="name" @pressed="pressed" class="green" />
+  <div v-if="pokemon">
+    <img :src="pokemon.sprites.back_default" alt="" />
+  </div>
+</template>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3 v-bind="$attrs">Hello From Component</h3>
+  <h5>Received name: {{ props.name }}</h5>
+  <button @click="getAlert">Press Me</button>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false
+};
+</script>
+
+<script lang="ts" setup>
+// import { useAttrs } from "vue";
+
+// const attrs = useAttrs();
+// console.log(attrs);
+
+const emits = defineEmits(["pressed"]);
+
+function getAlert() {
+  emits("pressed");
+}
+
+interface props {
+  name?: string;
+}
+const props = withDefaults(defineProps<props>(), {
+  name: "Stewart"
+});
+</script>
+
+<style>
+.green {
+  background-color: green;
+}
+</style>
